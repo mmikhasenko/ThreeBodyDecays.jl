1a7a78c4e91f086dceeb8f465748e7ff24a989b8
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-03-04 07:37:16 +0530

    Blatt-Weisskopf up to 7

--- src/lineshapes/formfactors.jl
+++ src/lineshapes/formfactors.jl
@@ -4,22 +4,15 @@ struct BlattWeisskopf{L} <: AbstractFlexFunc
     d::Float64
 end
 breakup(m, m1, m2) = sqrt((m - (m1 + m2)) * (m + (m1 + m2)) * (m - (m1 - m2)) * (m + (m1 - m2))) / 2m
-(bw::BlattWeisskopf{L})(p::Number) where {L} = error("BlattWeisskopf{L} is not defined for L>7")
-(bw::BlattWeisskopf{0})(p::Number) = one(p)
-(bw::BlattWeisskopf{1})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly1(z²)))
-(bw::BlattWeisskopf{2})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly2(z²)))
-(bw::BlattWeisskopf{3})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly3(z²)))
-(bw::BlattWeisskopf{4})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly4(z²)))
-(bw::BlattWeisskopf{5})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly5(z²)))
-(bw::BlattWeisskopf{6})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly6(z²)))
-(bw::BlattWeisskopf{7})(p::Number) = (z² = (bw.d * p)^2;
-sqrt(z² / chi_poly7(z²)))
+(ff::BlattWeisskopf{L})(p::Number) where {L} = error("BlattWeisskopf{L} is not defined for L>7")
+(ff::BlattWeisskopf{0})(p::Number) = one(p)
+(ff::BlattWeisskopf{1})(p::Number) = (z² = (ff.d * p)^2; sqrt(z² / chi_poly1(z²)))
+(ff::BlattWeisskopf{2})(p::Number) = (z² = (ff.d * p)^2; sqrt(z²^2 / chi_poly2(z²)))
+(ff::BlattWeisskopf{3})(p::Number) = (z² = (ff.d * p)^2; sqrt(z²^3 / chi_poly3(z²)))
+(ff::BlattWeisskopf{4})(p::Number) = (z² = (ff.d * p)^2; sqrt(z²^4 / chi_poly4(z²)))
+(ff::BlattWeisskopf{5})(p::Number) = (z² = (ff.d * p)^2; sqrt(z²^5 / chi_poly5(z²)))
+(ff::BlattWeisskopf{6})(p::Number) = (z² = (ff.d * p)^2; sqrt(z²^6 / chi_poly6(z²)))
+(ff::BlattWeisskopf{7})(p::Number) = (z² = (ff.d * p)^2; sqrt(z²^7 / chi_poly7(z²)))
 # 
 chi_poly0(z²) = one(z²)
 chi_poly1(z²) = 1 + z²
