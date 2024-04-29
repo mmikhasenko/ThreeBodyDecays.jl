2dd6afd0118eeeeb9427ba1c31664f4595f7ee23
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-04-29 18:38:09 +0200

    indenting

--- test/border_and_flatDalitz.jl
+++ test/border_and_flatDalitz.jl
@@ -2,11 +2,11 @@ using ThreeBodyDecays
 using Test
 
 @testset "Border and Flat Dalitz" begin
-	ms = ThreeBodyMasses(1.1, 1.3, 1.5; m0 = 6.0)
-	fDP = flatDalitzPlotSample(ms)
-	b = border(ms)
-	borderKibblemax = maximum(abs.(Kibble.(b, Ref(ms^2))))
-	phspKibblemax = maximum(abs.(Kibble.(fDP, Ref(ms^2))))
-	# 
-	@test borderKibblemax / phspKibblemax < 1e-7
+    ms = ThreeBodyMasses(1.1, 1.3, 1.5; m0=6.0)
+    fDP = flatDalitzPlotSample(ms)
+    b = border(ms)
+    borderKibblemax = maximum(abs.(Kibble.(b, Ref(ms^2))))
+    phspKibblemax = maximum(abs.(Kibble.(fDP, Ref(ms^2))))
+    # 
+    @test borderKibblemax / phspKibblemax < 1e-7
 end
