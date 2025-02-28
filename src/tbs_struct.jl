85d50404666dd0cc2d4f5624e225aed0e9ce0fa2
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-02-28 23:44:24 +0100

    add/fix docstrings

--- src/tbs_struct.jl
+++ src/tbs_struct.jl
@@ -168,6 +168,36 @@ Invariants(σ1, σ2, σ3) = MandelstamTuple{typeof(σ1)}((σ1, σ2, σ3))
 circleorigin(k, t) = (t[mod(k, 3)+1], t[mod(k + 1, 3)+1], t[mod(k - 1, 3)+1])
 
 fitin(y, (a, b)) = a + y * (b - a)
+
+
+
+
+"""
+    x2σs(x, ms::MassTuple; k::Int = last(findmin(Tuple(ms))))
+
+Maps a pair of variables `x` to a physical set of mandelstam invariants `σs` using a linear transformation.
+x[1] is mapped to σ[k], and x[2] is mapped into cosθij, that is used to compute σ[i] and σ[j].
+Uniform distribution of x does not lead to the phase space distribution of σs.
+
+## Arguments
+
+- `x` : a pair of numbers
+- `ms` : masses of the system as a `MassTuple`, see `ThreeBodyMasses`.
+- `k` : the index for which the variable is not generated. By default, the function picks the coordinates
+where the Dalitz plot has the closest shape to the squared fitting box.
+
+## Returns
+- an instance of `MandelstamTuple` with the squared masses.
+
+## Example
+
+The phase space sample with 100 points can be generated as follows:
+````julia
+σs0 = x2σs([0.3, 0.2], ms)
+````
+
+See also [`y2σs`](@ref).
+"""
 function x2σs(x, ms::MassTuple; k::Int)
     σk = fitin(x[1], lims(ms; k))
     σj = σjofk(2x[2] - 1, σk, ms^2; k)
@@ -181,9 +211,11 @@ end
 """
     y2σs(y, ms::MassTuple; k::Int = last(findmin(Tuple(ms))))
 
-Maps a pair of variables to the plain of squared masses using a linear transformation.
-The values [0,1] correspond to the physical limits of the mass squared variables.
-The physical value
+Maps a pair of variables to the plane of squared masses using a linear transformation,
+by shifting and scaling `y[1]` to `σ[i]`, and `y[2]` to `σ[j]`.
+The indices of variables are determined by the argument `k`.
+The mapping does not guarantee that values are physical, however,
+physical values of σs are phase-space distributed for uniform y.
 
 ## Arguments
 
@@ -193,25 +225,28 @@ The physical value
 where the Dalitz plot has the closest shape to the squared fitting box.
 
 ## Returns
+
 - an instance of `MandelstamTuple` with the squared masses.
-DecayChainLS docstring:
+
 ## Example
 
 The phase space sample with 100 points can be generated as follows:
-```julia
+````julia
 data = let
-N = 100
-# map random variables to dalitz
-_data = mapslices(rand(N,2); dims=2) do xy
-y2σs(xy, ms)
-end[:,1]
-# select physical
-filter!(_data) do σs
-isphysical(σs, ms)
-end
-_data
+    N = 100
+    # map random variables to dalitz
+    _data = mapslices(rand(N, 2); dims=2) do xy
+        y2σs(xy, ms)
+    end[:, 1]
+    # select physical
+    filter!(_data) do σs
+        isphysical(σs, ms)
+    end
+    _data
 end
 ````
+
+See also [`x2σs`](@ref).
 """
 function y2σs(y, ms::MassTuple; k::Int = last(findmin(Tuple(ms))))
     i, j = ij_from_k(k)
@@ -243,7 +278,7 @@ end
 #    _|_|
 
 """
-polardalitz2invariants(θ, expansion_point)
+    polardalitz2invariants(θ, expansion_point)
 
 For given polar angle θ, it returns an (σ1,σ2,σ3) Tuple of polynomials of radius r(θ) around the expansion point.
 The polynomial works as a function of the r coordinate.
